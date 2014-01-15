8dfa57ff604f9cf8f786281f4055575b23ed1df0
linzhanghui <linzhanghui@gmail.com>
2014-01-15 10:23:53 +0800

    form.class.php0.2

--- form.class.php
+++ form.class.php
@@ -7,6 +7,17 @@ class Form {
         $this->action = $action;
         $this->shape = isset($_GET["action"]) ? $_GET["action"] : "rect";
     }    
+    
+    function __tostring() {
+        $form='<form action="'.$this->action.'?action='.$this->shape.'"method="post">';
+        
+    }
 
+    private function getRect() {
+        $input ='<b>请输入 | 矩形 | 的宽度和高度：</b><p>';
+        $input .= '宽度：<input type="text" name="width" value="'.$_POST["width"].'"><br>';
+        $input .= '高度：<input type="text" name="height" value="'.$_POST["height"].'"><br>';
+        return $input;
+    }
 
 }

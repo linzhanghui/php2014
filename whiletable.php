d178e11c52cec21828e24f6269793097b6be5586
linzhanghui <linzhanghui@gmail.com>
2014-02-02 20:59:07 +0800

    whiletable

---
+++ whiletable.php
@@ -0,0 +1,29 @@
+<html>
+    <head><meta charset="utf-8"><title>使用while循环嵌套输出表格</title></head>
+    <body>
+        <table align="center" border="1" width=600>
+            <caption><h1>使用while循环嵌套输出表格</h1></caption>
+            <?php
+                $out=0;
+
+                while($out<10) {
+                    if($out%2==0)
+                        $bgcolor="#ffffff";
+                    else
+                        $bgcolor="#dddddd";
+
+                    echo "<tr bgcolor=".$bgcolor.">";
+                    $in=0;
+
+                    while($in<10) {
+                        echo "<td>".($out*10+$in)."</td>";
+                        $in++;
+                    }
+
+                    echo "</tr>";
+                    $out++;
+                }
+            ?>
+        </table>
+    </body>
+</html>

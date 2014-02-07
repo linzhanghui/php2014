85b97e22a3e953d7ad2e1f2bbff88c0b669958af
linzhanghui <linzhanghui@gmail.com>
2014-02-07 14:09:21 +0800

    postchange

---
+++ post.php
@@ -0,0 +1,8 @@
+<html>
+    <head><meta charset="utf-8"><title>标题</title></head>
+    <body>
+    <form method="post" action="viewthread.php" target="_blank">
+    
+    </form>
+    </body>
+</html>

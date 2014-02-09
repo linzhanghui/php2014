6bb6889fe8025da9039c493f249ec2ad06e1264e
linzhanghui <linzhanghui@gmail.com>
2014-02-09 22:16:43 +0800

    mailcheck_v0.1

--- mailcheck.php
+++ mailcheck.php
@@ -15,3 +15,13 @@ function mailcheck($email){
 
 //简单正则
 //  ^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$
+//
+<?php
+function isEmail($email){
+    if(preg_match("/^[0-9a-zA-Z]+@(([0-9a-zA-Z]+[.])+[a-z]{2,4}$/i",$email)){
+        return '邮件通过认证';
+    } else {
+        return '邮件未通过认证';
+    }
+}
+?>

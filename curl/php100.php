3ba8b147f8062d5e919e4e6545b68ab95dbc0dfa
linzhanghui <linzhanghui@gmail.com>
2014-02-28 13:50:06 +0800

    php100

--- curl/php100.php
+++ curl/php100.php
@@ -1,10 +1,34 @@
 <?php
-$login_url= "http://bbs.php100.com/login.php";
+
+/*
+$cookie_file    =    tempnam('./tmp','cookie');
+$login_url      =    "http://bbs.php100.com/login.php";
+$post_fields    =    '';
+
+
 $ch = curl_init($login_url);
+
+curl_setopt($ch,CURLOPT_POST);
+curl_setopt($ch,CURLOPT_POST,1); 
+curl_setopt($ch,CURLOPT_COOKIEJAR,$cookie_file);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$post_fields);
 
 
 
 curl_exec($ch);
 curl_close($ch);
+
+//-------------------
+$url="http://bbs.php100.com/index.php";
+$ch = curl_init($url);
+
+curl_setopt($ch,CURLOPT_);
+
+
+ */
+
+
+
+
 
 ?>

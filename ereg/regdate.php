89e3ddfa2f4b9cf47b41de02ec3e1695af6fb36f
linzhanghui <linzhanghui@gmail.com>
2014-03-04 10:19:56 +0800

    hello reg

---
+++ ereg/regdate.php
@@ -0,0 +1,9 @@
+<?php
+//数组顺序是不是不对呀？
+$date ="1990-09-09";
+if(ereg("([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})",$date,$regs)){
+    echo "$regs[3].$regs[2].$regs[1]";
+} else {
+    echo "Invalid date format: $date";
+}
+?>

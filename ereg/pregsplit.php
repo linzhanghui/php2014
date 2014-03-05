72487cb0c4fcae0f3dbdf9211f5189545a299cd3
linzhanghui <linzhanghui@gmail.com>
2014-03-05 14:23:01 +0800

    print_r

--- ereg/pregsplit.php
+++ ereg/pregsplit.php
@@ -1,9 +1,12 @@
 <?php
 $words = preg_split('/\d\./','my day:1. get up 2.get dressed 3.eat toast');
+/*
 echo $words[0]."</br>";
 echo "dd";
 
 echo $words[1]."</br>";
 echo $words[2]."</br>";
 echo $words[3]."</br>";
+ */
+print_r ($words);
 ?>

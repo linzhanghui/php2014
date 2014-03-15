59997f65f78de3d858ba9ab3aaab3263fd5ccf99
linzhanghui <linzhanghui@gmail.com>
2014-03-15 13:20:48 +0800

    after severals test

--- curl/liebiao.php
+++ curl/liebiao.php
@@ -1,11 +1,22 @@
 <?php
 
 
-$con = file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1746.shtml");
-$preg = "/<h1>·<a href='(.*)' target='_blank'>(.*)<\/a><span>/iUs";
-preg_match_all($preg,$con,$arr);
-print_r($arr);
+//$con = file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1746.shtml");
 
+
+$con = "<h1>·<a href='http://it.sohu.com/20140304/n396034271.shtml' target='_blank'>猫伙呆：空气水能发电电池组</a><span>"
+
+//$preg = "@<h1>·<a href='(*.)' target='_blank'>(*.)</a><span>(03/04 18:15)</span></h1>
+//<p>@";
+
+echo addslashes($con);
+
+
+/*
+$preg = "!<h1>·<a href='(.*)' target='_blank'>(.*)</a><span>!iUs";
+preg_match_all($preg,$con,$arr,PREG_SET_ORDER);
+print_r($arr);
+*/
 
 //print_r($con);
 ?>

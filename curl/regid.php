2efba3aeb0e1d35440bc72eb4298c6f0751a93f3
linzhanghui <linzhanghui@gmail.com>
2014-03-17 15:06:52 +0800

    somereg

--- curl/regid.php
+++ curl/regid.php
@@ -1,6 +1,22 @@
 <?php
+
 	$title = "Replacement Canon BP-511 CAMCORDER Battery [Item ID:3-236-523]";
 	if(eregi("(\[Item ID:)([0-9]+)-([0-9]+)-([0-9]+)(])",$title,$arr)){
 		print_r($arr);
 	}
+	
+	echo "</br>";
+	echo "</br>";
+
+
+	$date = "04.30.1973";
+	list($month, $day, $year) = split('[/.-]',$date);
+	echo "Month:$month; Day:$day; Year:$year<br />\n";
+
+	if(preg_match("/\sweb\w/i","PHP is the website scripting language of  choice.")){
+		print "A match was found.";
+	} else {
+		print "A match was not found.";
+	}
+
 ?>

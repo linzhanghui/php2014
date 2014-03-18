eeb914a6feeb1d518b044f0caf03e274722143e7
linzhanghui <linzhanghui@gmail.com>
2014-03-18 11:42:45 +0800

    can replacehref

--- curl/replaceherf.php
+++ curl/replaceherf.php
@@ -1,8 +1,12 @@
 <?php
 	$content = "<a href=http://www.163.com>neteasy</a>";
-	echo $content;
-	ereg_replace('<a([^>]*)([^<]*)</a>','<font color="red">\\2</font>',$content);
-	ereg_replace("<a [^>]*>|<\/a>","",$content);
+//	preg_match_all('/<a[^>]*>[^<]*</a>/i',$content,$matches);
+
+//	print_r($matches[0]);	
+
+//	echo $content;
+//	preg_replace('/<a([^>]*)([^<]*)<\/a>/','<font color="red">\\2</font>',$content);
+	$content = preg_replace("/<a[^>]*>|<\/a>/","",$content);
 	echo $content;
 //	ereg_
 ?>

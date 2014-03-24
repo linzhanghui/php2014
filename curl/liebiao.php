af386e1e2a8ea88e1b989d518976f6248d9c8f80
linzhanghui <linzhanghui@gmail.com>
2014-03-24 11:02:38 +0800

    success fetch content from sohu

--- curl/liebiao.php
+++ curl/liebiao.php
@@ -1,22 +1,20 @@
 <?php
 
 
-//$con = file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1746.shtml");
+$con = file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1746.shtml");
 
 
-$con = "<h1>·<a href='http://it.sohu.com/20140304/n396034271.shtml' target='_blank'>猫伙呆：空气水能发电电池组</a><span>"
 
-//$preg = "@<h1>·<a href='(*.)' target='_blank'>(*.)</a><span>(03/04 18:15)</span></h1>
-//<p>@";
+//$preg = "#<h1>·<a href='(.*)' target='_blank'>(.*)</a><span>#iUs";
+//$preg = "#<title>(.*)</title>#iUs";
+//$preg = "#<h1>·<a href='(.*)' target='_blank'>一名90后眼中的游戏世界</a><span>#iUs";
+$preg ="#<h1>(.*)</h1>#iUs";
 
-echo addslashes($con);
-
-
-/*
-$preg = "!<h1>·<a href='(.*)' target='_blank'>(.*)</a><span>!iUs";
-preg_match_all($preg,$con,$arr,PREG_SET_ORDER);
+//$preg = "!<h1>·<a href='(.*)' target='_blank'>(.*)</a><span>!iUs";
+preg_match_all($preg,$con,$arr);
 print_r($arr);
-*/
+
 
 //print_r($con);
+
 ?>

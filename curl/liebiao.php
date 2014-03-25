c5e509fbaa01cda46e331bc24a3dd1577306c555
linzhanghui <linzhanghui@gmail.com>
2014-03-25 15:46:06 +0800

    liebiao

--- curl/liebiao.php
+++ curl/liebiao.php
@@ -1,16 +1,18 @@
 <?php
 header("Content-Type: text/html; charset=GBK");
-$con = file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1746.shtml");
-$con .= file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1747.shtml");
-$con .= file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1748.shtml");
-$con .= file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1749.shtml");
+if($_GET[yid]<=60){
+
+$con = file_get_contents("http://it.sohu.com/7/1002/17/column203661721_17".$_GET[yid].".shtml");
+//$con .= file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1747.shtml");
+//$con .= file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1748.shtml");
+//$con .= file_get_contents("http://it.sohu.com/7/1002/17/column203661721_1749.shtml");
 
 
 
 //$preg = "#<h1>·<a href='(.*)' target='_blank'>(.*)</a><span>#iUs";
 //$preg = "#<title>(.*)</title>#iUs";
 //$preg = "#<a href='(.*)' target='_blank'>一名90后眼中的游戏世界</a><span>#iUs";
-$preg = "/<a href='(.*)' target='_blank'>(.*)<\/a><span>/iUs";
+$preg = "/<a href='(.*)' target='_blank'>(.*)<\/a><span>/i";
 //$preg ="#<h1>(.*)</h1>#iUs";
 
 //$preg = "!<h1>·<a href='(.*)' target='_blank'>(.*)</a><span>!iUs";
@@ -20,7 +22,12 @@ preg_match_all($preg,$con,$arr);
 
 foreach($arr[1] as $id=>$v){
 //	echo $v."<br>".$arr[2];
-	echo $v." ".$arr[2][$id]."<br>";
+	echo "<a href=neirong.php?url=".$v.">".$arr[2][$id]."</a><br>";
+//	echo $v." ".$arr[2][$id]."<br>";
+}
+
+	$_GET[yid]++;
+	echo "<script>location.href='liebiao.php?yid=".$_GET[yid]."'</script>";
 }
 
 //print_r($con);

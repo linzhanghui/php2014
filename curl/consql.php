cf072e6e7682b04908e18fa480efa846ef3b52e3
linzhanghui <linzhanghui@gmail.com>
2014-03-26 11:22:30 +0800

    leibiaoneirong

--- curl/consql.php
+++ curl/consql.php
@@ -1,6 +1,7 @@
 <?php
 	mysql_connect("localhost","root","123456");
 	mysql_select_db("test");
+	mysql_query("set names 'GBK'");
 	
 
 
--- curl/neirong.php
+++ curl/neirong.php
@@ -1,19 +1,39 @@
 <?php
 
 header("Content-Type: text/html; charset=GBK");
+include_once("consql.php");
+$gid = (int)$_GET[id];
 
+$sql = "select * from tmp_url where id='$gid'";
+$q = mysql_query($sql);
 
-$con = file_get_contents("http://it.sohu.com/20140304/n396033871.shtml");
+$row = mysql_fetch_array($q);
+
+echo $row[0].$row[1]."<br>";
+
+//$con = file_get_contents($row[url]);
 //$preg = "#<title>(.*)</title>#iUs";
 
 //preg_match($preg,$con,$arr);
 
 //print_r($arr);
 
+$sql2="select * from tmp_url where id>'$gid' order by id asc limit 1";
 
-echo zz("#<title>(.*)</title#iUs",$con);
-echo "<br>";
-echo zz("/<div class=\"text clear\" id=\"contentText\”>(.*)<span class=\"editer\">/",$con);
+$q2 =mysql_query($sql2);
+
+$result = mysql_fetch_array($q2);
+
+echo $result[0].$result[1]."<br>";
+
+if ($result[0]){
+echo "<script>location.href='neirong.php?id=".$result[0]."'</script>";
+}
+
+
+//echo zz("#<title>(.*)</title#iUs",$con);
+//echo "<br>";
+//echo zz("#<SOHUADCODE>(.*)<div style=\"display:none;\">#is",$con);
 
 function zz($preg,$con,$num=1){
 

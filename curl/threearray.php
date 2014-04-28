a5769d3e8baf176f6c6edcd3e8324d814d13de26
linzhanghui <linzhanghui@gmail.com>
2014-04-28 11:23:44 +0800

    finishthreearray

--- curl/threearray.php
+++ curl/threearray.php
@@ -7,20 +7,29 @@
 		),
 		"productdept"=>array(
 			array(1,"Mr.li","manager",5000),
-			array(1,"Mr.tao",5000),
-			array(1,"Mr.wu","manager",5000),
+			array(2,"Mr.tao","member",2800),
+			array(3,"wu","member",4000),
 		),
 		"moneydept"=>array(
-
+			array(1,"Mr.lin","manager",58000),
+			array(2,"Mr.li","member",51000),
+			array(3,"Mr.linux","member",55000),
 		)
 	);
 
 	foreach($wage as $sector =>$table) {
 		echo '<table border="1" width="600" align="center">';
 		echo '<caption><h2>'.$sector.'10月份工资表</h2></caption>';
-		
-
-
+		echo '<tr bgcolor="#dddddd">';		
+		echo '<th>编号</th><th>姓名</th><th>职务</th><th>工资</th>';
+		echo '</tr>';
+		foreach($table as $row){
+			echo '<tr>';
+			foreach($row as $col){
+				echo '<td>'.$col.'</td>';
+			}
+			echo '</tr>';
+		}
 		echo '</table><br>';
 
 	}
